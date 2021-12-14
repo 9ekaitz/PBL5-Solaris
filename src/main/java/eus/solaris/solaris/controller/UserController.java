a71e1673aa354f3a7184cc267f03ae7e64a8baa4
gorka.lertxundi <gorka.lertxundi@alumni.mondragon.edu>
2021-12-14 12:27:09 +0100

    pipeline test 3

--- src/main/java/eus/solaris/solaris/controller/UserController.java
+++ src/main/java/eus/solaris/solaris/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
     public String get(@RequestParam String name, Model model) {
         User user = userRepository.findByUsername(name);
         model.addAttribute("user", user);
-        System.out.println("get user 2");
+        System.out.println("get user 3");
         return "user";
     }
 
