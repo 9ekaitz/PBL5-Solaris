6ed94f2adebf1fe7a565bcd4be943d10f3dbe88a
gorka.lertxundi <gorka.lertxundi@alumni.mondragon.edu>
2021-12-16 08:49:24 +0100

    code smells fixed

--- src/main/java/eus/solaris/solaris/security/WebSecurityConfig.java
+++ src/main/java/eus/solaris/solaris/security/WebSecurityConfig.java
@@ -47,36 +47,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserService userService;
 
-	private String test1;
-	private String test2;
-	private String test3;
-	private String test4;
-	private String test5;
-	private String test6;
-	private String test7;
-	private String test8;
-	private String test9;
-	private String test10;
-	private String test11;
-	private String test12;
-	private String test13;
-	private String test14;
-	private String test15;
-	private String test16;
-	private String test17;
-	private String test18;
-	private String test19;
-	private String test20;
-	private String test21;
-	private String test22;
-	private String test23;
-	private String test24;
-	private String test25;
-	private String test26;
-	private String test27;
-	private String test28;
-
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
