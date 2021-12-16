887da6d1ad1918b01aa2489f1d12a170ce79e48c
gorka.lertxundi <gorka.lertxundi@alumni.mondragon.edu>
2021-12-16 11:46:58 +0100

    cd preparation

--- src/main/java/eus/solaris/solaris/SolarisApplication.java
+++ src/main/java/eus/solaris/solaris/SolarisApplication.java
@@ -2,9 +2,10 @@ package eus.solaris.solaris;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SolarisApplication {
+public class SolarisApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SolarisApplication.class, args);
