802193e08cea5709704f55ec567acd810e876b3f
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2022-01-04 10:23:16 +0100

    Task entity created

---
+++ src/main/java/eus/solaris/solaris/domain/Task.java
@@ -0,0 +1,36 @@
+package eus.solaris.solaris.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Version;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Task {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column
+  private String description;
+
+  @Column
+  private Boolean completed = false;
+
+  @ManyToOne
+  @JoinColumn(name = "installation", nullable = false)
+  private Installation installation;
+
+  @Version
+  private Integer version;
+}
