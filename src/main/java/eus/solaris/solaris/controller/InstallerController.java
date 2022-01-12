a150cd4719eaaa42af84720bea4f0b5f9a57fee5
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2022-01-12 12:12:49 +0100

    privilege based preauth added

--- src/main/java/eus/solaris/solaris/controller/InstallerController.java
+++ src/main/java/eus/solaris/solaris/controller/InstallerController.java
@@ -3,14 +3,13 @@ package eus.solaris.solaris.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import eus.solaris.solaris.domain.Role;
 import eus.solaris.solaris.domain.Installation;
 import eus.solaris.solaris.domain.User;
 import eus.solaris.solaris.service.InstallationService;
@@ -26,17 +25,11 @@ public class InstallerController {
   @Autowired
   InstallationService installationService;
 
-
+  @PreAuthorize("hasAuthority('AUTH_INSTALL_VIEW')")
   @GetMapping
   public String showInstallerPage(Authentication authentication, Model model) {
     if (authentication == null) return "page/login";
     User user = userService.findByUsername(authentication.getName());
-
-    /* TODO: Remove manually setted role */
-    Role role = new Role();
-    role.setName("admin");
-    user.setRole(role);
-    /* --------------------------------- */
 
     List<Installation> pendingInstallations = installationService.findByInstallerAndCompleted(user, false);
     List<Installation> completedInstallations = installationService.findByInstallerAndCompleted(user, true);
@@ -46,10 +39,4 @@ public class InstallerController {
   
     return "page/installer";
   }
-
-  @GetMapping(value = "/task/{id}")
-  public String showTask(@PathVariable(value = "id") Long id){
-    return "";
-  }
-
 }
