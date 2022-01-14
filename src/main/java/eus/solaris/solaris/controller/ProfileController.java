11647cd4720f76e60dc4317b841984ef52499d0a
Aritz Domaika <aritz.domaika@alumni.mondragon.edu>
2022-01-14 09:18:34 +0100

    i18n fixes

--- src/main/java/eus/solaris/solaris/controller/ProfileController.java
+++ src/main/java/eus/solaris/solaris/controller/ProfileController.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
+// import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -31,7 +31,7 @@ import eus.solaris.solaris.service.ProvinceService;
 import eus.solaris.solaris.service.UserService;
 
 @Controller
-@PreAuthorize("hasRole('ROLE_USER')")
+// @PreAuthorize("hasRole('ROLE_USER')")
 public class ProfileController {
 
     static final String SUCCESS_ATTRIBUTE = "success";
