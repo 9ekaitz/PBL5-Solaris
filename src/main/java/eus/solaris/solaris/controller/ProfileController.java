19f7a6279282f4c5836555e94e8269a0d2ba0b80
Aritz Domaika <aritz.domaika@alumni.mondragon.edu>
2022-01-16 17:05:16 +0100

    cambio de carpeta

--- src/main/java/eus/solaris/solaris/controller/ProfileController.java
+++ src/main/java/eus/solaris/solaris/controller/ProfileController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -31,6 +32,7 @@ import eus.solaris.solaris.service.ProvinceService;
 import eus.solaris.solaris.service.UserService;
 
 @Controller
+@PreAuthorize("hasAuthority('USER_LOGGED_VIEW')")
 public class ProfileController {
 
     static final String SUCCESS_ATTRIBUTE = "success";
---
+++ src/test/java/eus/solaris/solaris/config/SpringWebAuxTestConfig.java
@@ -0,0 +1,57 @@
+package eus.solaris.solaris.config;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import eus.solaris.solaris.domain.Privilege;
+import eus.solaris.solaris.domain.Role;
+import eus.solaris.solaris.domain.User;
+import eus.solaris.solaris.security.CustomUserDetails;
+
+@TestConfiguration
+public class SpringWebAuxTestConfig {
+
+    Role ROLE_ADMIN = new Role(1L, "ROLE_ADMIN", true, null, Stream
+            .of(new Privilege(1L, "AUTH_INSTALL_VIEW", "auth.install.view", true, null, 1))
+            .collect(Collectors.toSet()), 1);
+
+    Role ROLE_USER = new Role(2L, "ROLE_USER", true, null, Stream
+            .of(new Privilege(2L, "USER_LOGGED_VIEW", "user.logged.view", true, null, 1))
+            .collect(Collectors.toSet()), 1);
+
+    User basicUser = new User(1L, "testyUser", "testy@foo", "foo123", "Testy", "Tester", "User", true, null, null, ROLE_USER, null, 1);
+
+    User technicianUser = new User(2L, "testyTechnician", "testy@foo", "foo123", "Testy", "Tester", "Technician", true, null, null,
+            null, null, 1);
+
+    User supervisorUser = new User(3L, "testySupervisor", "testy@foo", "foo123", "Testy", "Tester", "Supervisor", true, null, null,
+            null, null, 1);
+
+    User adminUser = new User(4L, "testyAdmin", "testy@foo", "foo123", "Testy", "Tester", "Admin", true, null, null, ROLE_ADMIN,
+            null, 1);
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetailsService userDetailsService = new UserDetailsService() {
+
+            @Override
+            public UserDetails loadUserByUsername(String username) {
+                CustomUserDetails userDetails;
+                switch (username){
+                    case "testyAdmin": userDetails = new CustomUserDetails(adminUser); break;
+                    case "testySupervisor": userDetails = new CustomUserDetails(supervisorUser); break;
+                    case "testyTechnician": userDetails = new CustomUserDetails(technicianUser); break;
+                    case "testyUser": userDetails = new CustomUserDetails(basicUser); break;
+                    default: userDetails = new CustomUserDetails(basicUser); break;
+                }
+                return userDetails;
+            }
+        };
+        return userDetailsService;
+    }
+}
