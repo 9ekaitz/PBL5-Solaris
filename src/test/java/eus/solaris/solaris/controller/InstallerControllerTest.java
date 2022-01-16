1c8b32a7257bbd47b3583b5a9c83f4ca2b8f7306
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2022-01-16 10:11:37 +0100

    some imports removed

--- src/test/java/eus/solaris/solaris/controller/InstallerControllerTest.java
+++ src/test/java/eus/solaris/solaris/controller/InstallerControllerTest.java
@@ -1,12 +1,8 @@
 package eus.solaris.solaris.controller;
 
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
@@ -15,13 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import eus.solaris.solaris.config.SpringWebAuxTestConfig;
-import eus.solaris.solaris.domain.Privilege;
 import eus.solaris.solaris.domain.Role;
 import eus.solaris.solaris.domain.User;
 import eus.solaris.solaris.security.UserDetailsServiceImpl;
@@ -55,6 +48,8 @@ public class InstallerControllerTest {
   @MockBean
   UserDetailsServiceImpl userDetailsServiceImpl;
 
+  Role ROLE_ADMIN;
+
   @Test
   public void accessToInstallPageWithoutCredentials() {
     try {
@@ -65,22 +60,16 @@ public class InstallerControllerTest {
   }
 
   @Before
-  public void loadAdminUser() {
-    Role ROLE_ADMIN = new Role(1L, "ROLE_TEST", true, null, Stream
-        .of(new Privilege(1L, "AUTH_INSTALL_VIEW", "auth.install.view", true, null, 1))
-        .collect(Collectors.toSet()), 1);
-    User adminUser = new User(4L, "testyAdmin", "testy@foo", "foo123", "Testy", "Tasty", "Admin", true, ROLE_ADMIN,
-        null, 1);
-    when(userServiceImpl.findByUsername("adminUser")).thenReturn(adminUser);
+  void loadUser(){
+    String username = "testy";
+    User user = new User(1L, "testy", "tetsy@email.com", "testy123", "Testy", "Tasty", "Tester", true, null, null, 1);
+    when(userServiceImpl.findByUsername(username)).thenReturn(user);
   }
+  
 
   @Test
-  @WithUserDetails("adminUser")
-  public void accessToInstallPageWithWrongCredentials() {
-    try {
+  @WithMockUser(authorities = {"AUTH_INSTALL_VIEW"})
+  public void accessToInstallPageWithWrongCredentials() throws Exception{
       mvc.perform(get("https://localhost/install")).andExpect(status().isFound());
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 }
