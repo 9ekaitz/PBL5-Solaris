41fdfb4b600da871328a100afa323418748e931b
Aritz Domaika <aritz.domaika@alumni.mondragon.edu>
2022-01-17 17:52:55 +0100

    password validator

--- src/test/java/eus/solaris/solaris/validation/PasswordConstraintValidatorTest.java
+++ src/test/java/eus/solaris/solaris/validation/PasswordConstraintValidatorTest.java
@@ -2,9 +2,11 @@ package eus.solaris.solaris.validation;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -13,9 +15,36 @@ public class PasswordConstraintValidatorTest {
     @InjectMocks
     private PasswordConstraintValidator passwordConstraintValidator;
 
+    @Mock
+    private ValidPassword constraintAnnotation;
+
+    @BeforeEach
+    void initialize(){
+        passwordConstraintValidator.initialize(constraintAnnotation);
+    }
+
+
     @Test
-    void testPasswordValidation() {
-        String password = "Ikastola2.@";
+    void testPasswordValidationValid() {
+        String password = "Password.@";
+        assertEquals(true, passwordConstraintValidator.isValid(password, null));
+    }
+
+    @Test
+    void testPasswordValidationInvalid() {
+        String password = "admin";
+        assertEquals(true, passwordConstraintValidator.isValid(password, null));
+    }
+
+    @Test
+    void testPasswordValidationNull() {
+        String password = null;
+        assertEquals(true, passwordConstraintValidator.isValid(password, null));
+    }
+
+    @Test
+    void testPasswordValidationEmpty() {
+        String password = "";
         assertEquals(true, passwordConstraintValidator.isValid(password, null));
     }
     
