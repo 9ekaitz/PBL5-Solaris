8ecf041fbc4b3268eea7c976d2a0b5d5b523dab7
UrkoAT <urko.agirregomezkort@alumni.mondraon.edu>
2022-01-18 08:24:04 +0100

    Capitalize lmao

--- src/main/java/eus/solaris/solaris/service/multithreading/conversions/ConversionToEur.java
+++ src/main/java/eus/solaris/solaris/service/multithreading/conversions/ConversionToEur.java
@@ -2,32 +2,7 @@ package eus.solaris.solaris.service.multithreading.conversions;
 
 public class ConversionToEUR implements IConversion {
 
-    // private static final String URL =
-    // "https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?";
     private static final Double AVERAGE_FACTOR = 0.2533;
-
-    // private Map<String, String> params;
-    // private Map<String, String> headers;
-
-    // private Double factor;
-
-    /*
-     * public ConversionToEUR() {
-     * this.params = new HashMap<>();
-     * LocalDateTime start = LocalDate.now().atStartOfDay();
-     * LocalDateTime end = LocalDate.now().atTime(23, 59, 59);
-     * params.put("start_date", start.truncatedTo(ChronoUnit.MINUTES).toString());
-     * params.put("end_date", end.truncatedTo(ChronoUnit.MINUTES).toString());
-     * this.headers = new HashMap<>();
-     * 
-     * try {
-     * String jsonString = HTTPRequest.getRequest(URL, params, headers);
-     * } catch (Exception e) {
-     * e.printStackTrace();
-     * factor = AVERAGE_FACTOR;
-     * }
-     * }
-     */
 
     @Override
     public Double apply(Double t) {
