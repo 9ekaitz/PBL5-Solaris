a99dfcd83eefbd9fb466b09d1b82b1ab122dc28f
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2022-01-21 17:07:42 +0100

    bug fixes + license in the head of the files

--- src/main/java/eus/solaris/solaris/util/Functions.java
+++ src/main/java/eus/solaris/solaris/util/Functions.java
@@ -1,3 +1,27 @@
+/***********************************************************************************
+* MIT License
+* 
+* Copyright (c) 2021 boringdesigners
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+***********************************************************************************/
+
 package eus.solaris.solaris.util;
 
 public class Functions {
@@ -19,11 +43,11 @@ public class Functions {
   }
 
   public static int getDigit(int number, int ntn)  {
-    return (int) Math.floor(((double)number/ Math.pow(10, ntn)) % 10);
+    return (int) Math.floor((number/ Math.pow(10, ntn)) % 10);
   }
 
   public static Boolean getBoolean(int number, int ntn) {
-    return ((getDigit(number, ntn) % 2) == 0? true : false);
+    return (getDigit(number, ntn) % 2) == 0;
   }
 
   public static Double getAngle(Double x, Double y) {
