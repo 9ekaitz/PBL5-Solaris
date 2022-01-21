4821b8ce8ea060adbe82d2b7f028a95af8208df6
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2022-01-21 14:16:47 +0100

    Beam and Functions classes cretaed to generate the profile picture svg

---
+++ src/main/java/eus/solaris/solaris/util/Beam.java
@@ -0,0 +1,123 @@
+package eus.solaris.solaris.util;
+
+
+public class Beam {
+
+  private static final int SIZE = 36;
+  private static final String[] COLORS = {"#F9AA31","#FA8832","#FACC32","#FAEE32","#E3FA32","#C1FA32"};
+
+  private Data generateData(String name, String[] colors) {
+    Data data = new Data();
+    
+    int numFromName = Functions.getHashCode(name);
+    int range = colors.length;
+    String wrapperColor = Functions.getRandomColor(numFromName, colors, range);
+    int preTranslateX = Functions.getUnit(numFromName, 10, 1);
+    int wrapperTranslateX = (preTranslateX < 5 ? preTranslateX + SIZE / 9 : preTranslateX);
+    int preTranslateY = Functions.getUnit(numFromName, 10, 2);
+    int wrapperTranslateY = (preTranslateY < 5 ? preTranslateY + SIZE / 9 : preTranslateY);
+
+    data.wrapperColor = wrapperColor;
+    data.faceColor = Functions.getContrast(wrapperColor);
+    data.backgroundColor = Functions.getRandomColor(numFromName + 13, colors, range);
+    data.wrapperTranslateX = wrapperTranslateX;
+    data.wrapperTranslateY = wrapperTranslateY;
+    data.wrapperRotate = Functions.getUnit(numFromName, 360, 0);
+    data.wrapperScale = 1 + Functions.getUnit(numFromName, SIZE, 0) / 10;
+    data.isMouthOpen = Functions.getBoolean(numFromName, 2);
+    data.isCircle = Functions.getBoolean(numFromName, 1);
+    data.eyeSpread = Functions.getUnit(numFromName, 5, 0);
+    data.mouthSpread= Functions.getUnit(numFromName, 3, 0);
+    data.faceRotate = Functions.getUnit(numFromName, 10, 3);
+    data.faceTranslateX = (wrapperTranslateX > SIZE / 6 ? wrapperTranslateX / (double)2 : Functions.getUnit(numFromName, 8, 1));
+    data.faceTranslateY = (wrapperTranslateY > SIZE / 6 ? wrapperTranslateY / (double)2 : Functions.getUnit(numFromName, 7, 2));
+
+    return data;
+  }
+
+  public String getAvatarBeam(String name, int size, boolean square){
+    Data data = generateData(name, COLORS);
+
+    return "<svg " +
+      "viewBox=\"0 0 " + SIZE + " " + SIZE+"\" "+
+      "fill=\"none\" " +
+      "role=\"img\" " +
+      "xmlns=\"http://www.w3.org/2000/svg\" " +
+      "width=\""+size+"\" " +
+      "height=\""+size+"\" " +
+    ">" +
+    "<mask id=\"mask__beam\" maskUnits=\"userSpaceOnUse\" x=\"0\" y=\"0\" width=\""+SIZE+"\" height=\""+SIZE+"\">" +
+        "<rect width=\""+SIZE+"\" height=\""+SIZE+"\" rx=\""+(square ?  "undefined" : SIZE * 2 )+"\" fill=\"#FFFFFF\" />" +
+      "</mask>" +
+      "<g mask=\"url(#mask__beam)\">" +
+        "<rect width=\""+ SIZE +"\" height=\""+SIZE+"\" fill=\""+data.backgroundColor+"\" /> " +
+        "<rect " +
+          "x=\"0\" " +
+          "y=\"0\" " +
+          "width=\""+SIZE+"\" " +
+          "height=\""+SIZE+"\" " +
+          "transform=\" " +
+            "translate("+data.wrapperTranslateX+" "+data.wrapperTranslateY +
+            ") rotate("+data.wrapperRotate+" "+(SIZE / 2)+" "+(SIZE / 2) +
+            ") scale("+data.wrapperScale+")"+"\" " +
+          "fill=\""+data.wrapperColor+"\" " +
+          "rx=\""+(data.isCircle ? SIZE : SIZE / 6)+"\" " +
+        "/>" +
+        "<g " +
+          "transform=\"" +
+            "translate("+data.faceTranslateX+" "+data.faceTranslateY +
+              ") rotate("+data.faceRotate+" "+(SIZE / 2)+" "+(SIZE / 2)+")\" " +
+        ">" +
+        (data.isMouthOpen ? (
+                      "<path " +
+                        "d=\"M15 "+(19 + data.mouthSpread)+"c2 1 4 1 6 0\" " +
+                        "stroke=\""+data.faceColor+"\" " +
+                        "fill=\"none\" " +
+                        "strokeLinecap=\"round\" " +
+                      "/>"
+                    ) : (
+                      "<path " +
+                        "d=\"M13, "+(19 + data.mouthSpread)+" a1,0.75 0 0,0 10,0\" " +
+                        "fill=\""+data.faceColor+"\" " +
+                      "/>"
+                    )) +
+                    "<rect " +
+                      "x=\""+(14 - data.eyeSpread)+"\" " +
+                      "y=\"14\" " +
+                      "width=\"1.5\" " +
+                      "height=\"2\" " +
+                      "rx=\"1\" " +
+                      "stroke=\"none\" " +
+                      "fill=\""+data.faceColor+"\" " +
+                    "/>" +
+                    "<rect " +
+                      "x=\""+(20 + data.eyeSpread)+"\" " +
+                      "y=\"14\" " +
+                      "width=\"1.5\" " +
+                      "height=\"2\" " +
+                      "rx=\"1\" " +
+                      "stroke=\"none\" " +
+                      "fill=\""+data.faceColor+"\" " +
+                    "/>" +
+                  "</g>" +
+                "</g>" +
+              "</svg>";
+  }
+
+private class Data{
+  String wrapperColor;
+  String faceColor;
+  String backgroundColor;
+  double wrapperTranslateX;
+  double wrapperTranslateY;
+  int wrapperRotate;
+  int wrapperScale;
+  boolean isMouthOpen;
+  boolean isCircle;
+  int eyeSpread;
+  int mouthSpread;
+  int faceRotate;
+  double faceTranslateX;
+  double faceTranslateY;
+}
+}
---
+++ src/main/java/eus/solaris/solaris/util/Functions.java
@@ -0,0 +1,62 @@
+package eus.solaris.solaris.util;
+
+public class Functions {
+
+  public static int getHashCode(String name){
+    int hash = 0;
+    for (int i = 0; i < name.length(); i++){
+      int character = name.charAt(i);
+      hash = ((hash << 5)-hash)+character;
+      hash = hash & hash;
+    }
+    return Math.abs(hash);
+  }
+
+  public static int getModulus(int num, int max) {
+    return num % max;
+  }
+
+  public static int getDigit(int number, int ntn)  {
+    return (int) Math.floor(((double)number/ Math.pow(10, ntn)) % 10);
+  }
+
+  public static Boolean getBoolean(int number, int ntn) {
+    return ((getDigit(number, ntn) % 2) == 0? true : false);
+  }
+
+  public static Double getAngle(Double x, Double y) {
+    return Math.atan2(y, x) * 180 / Math.PI;
+  }
+
+  public static int getUnit(int number, int range, int index) {
+    int value = number % range;
+  
+    if(index > 0 && ((getDigit(number, index) % 2) == 0)) {
+      return -value;
+    } else return value;
+  }
+
+  public static String getRandomColor(int number, String[] colors, int range) {
+    return colors[number % range];
+  }
+  
+  public static String getContrast (String hexcolor) {
+
+    // If a leading # is provided, remove it
+    if (hexcolor.substring(0, 1).equals("#")) {
+      hexcolor = hexcolor.substring(1);
+    }
+  
+    // Convert to RGB value
+    int r = Integer.parseInt(hexcolor.substring(0,2),16);
+    int g = Integer.parseInt(hexcolor.substring(2,4),16);
+    int b = Integer.parseInt(hexcolor.substring(4,6),16);
+  
+    // Get YIQ ratio
+    int yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+  
+    // Check contrast
+    return (yiq >= 128) ? "#000000" : "#FFFFFF";
+  }
+
+}
