f5dcd63ab2956a805136b79be7190d3690621261
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2022-01-25 12:30:39 +0100

    oribe fix

--- src/main/java/eus/solaris/solaris/repository/specifications/ProductSpecifications.java
+++ src/main/java/eus/solaris/solaris/repository/specifications/ProductSpecifications.java
@@ -9,18 +9,18 @@ import eus.solaris.solaris.domain.Product;
 public class ProductSpecifications {
 
     public static Specification<Product> findByBrandIds(List<Long> brandIds) {
-        return (product, cq, cb) -> product.get("brand").get("id").in(brandIds);
+        return (product, cq, cb) -> product.get("model").get("brand").get("id").in(brandIds);
     }
 
     public static Specification<Product> findByMaterialIds(List<Long> materialIds) {
-        return (product, cq, cb) -> product.get("material").get("id").in(materialIds);
+        return (product, cq, cb) -> product.get("model").get("material").get("id").in(materialIds);
     }
 
     public static Specification<Product> findByColorIds(List<Long> colorId) {
-        return (product, cq, cb) -> product.get("color").get("id").in(colorId);
+        return (product, cq, cb) -> product.get("model").get("color").get("id").in(colorId);
     }
 
     public static Specification<Product> findBySizeIds(List<Long> sizeIds) {
-        return (product, cq, cb) -> product.get("size").get("id").in(sizeIds);
+        return (product, cq, cb) -> product.get("model").get("size").get("id").in(sizeIds);
     }
 }
