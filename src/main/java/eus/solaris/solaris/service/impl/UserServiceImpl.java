4a439198abe2b702cfe68b8ab05b9ac01a474288
Oribe <56338499+Orobe@users.noreply.github.com>
2022-01-25 11:21:27 +0100

    #11 username case ignore (#30)

--- src/main/java/eus/solaris/solaris/service/impl/UserServiceImpl.java
+++ src/main/java/eus/solaris/solaris/service/impl/UserServiceImpl.java
@@ -66,7 +66,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findByUsername(String username) {
-        return userRepository.findByUsername(username);
+        return userRepository.findByUsernameIgnoreCase(username);
     }
 
     @Override
